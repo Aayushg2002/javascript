8ed9b34a4c603518d92a676ab45c11aab578a0f4
Ayush Gupta <123961387+Aayushg2002@users.noreply.github.com>
2024-07-01 17:07:17 +0000

    variable

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 12345
+let accountEmail = "Ayush@gmail.com"
+var accountPassword = "99999"
+accountCity="Banglore"
+let accountState;
+
+// accountId = 123 //Not allowed
+
+accountEmail = "Ayush@google.com"
+accountPassword = "9898989"
+accountcity="Agra"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountcity, accountState])
